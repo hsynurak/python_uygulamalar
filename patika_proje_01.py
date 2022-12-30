fe047e6370d395e70e83e153c46b0aa67ffefba4
huseyinurak <102410337+hsynurak@users.noreply.github.com>
2022-12-30 20:31:10 +0300

    Update patika_proje_01.py

--- patika_proje_01.py
+++ patika_proje_01.py
@@ -23,5 +23,26 @@ def flatten(list_name):
 
 print(flatten(list_1))
 
+#****************************************************************************************************************************************************
 
+"""
+Verilen listenin içindeki elemanları tersine döndüren bir fonksiyon yazın.
+Eğer listenin içindeki elemanlar da liste içeriyorsa onların elemanlarını da tersine döndürün.
+Örnek olarak:
+input: [[1, 2], [3, 4], [5, 6, 7]]
+output: [[[7, 6, 5], [4, 3], [2, 1]]
+"""
+
+
+
+list_2 = [[1, 2], [3, 4], [5, 6, 7]]
+
+def reverse_list(l):
+    l.reverse()
+    for sublist in l:
+        if type(sublist) == list:
+            reverse_list(sublist)
+
+reverse_list(list_2)
+print(list_2)
 
